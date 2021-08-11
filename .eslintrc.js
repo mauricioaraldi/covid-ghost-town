55faa26a6a940eec0ae780e8a85978bdb0e79c63
Mauricio Araldi <araldi.mauricio@gmail.com>
2021-08-11 18:20:04 +0200

    Migrated to NextJS

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
+    'next',
   ],
   globals: {
     Atomics: 'readonly',
---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTranslation } from 'next-i18next';
+
+export default function Home() {
+  const { t } = useTranslation('common');
+
+  return (
+    <div>{t('test')}</div>
+  );
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+    },
+  };
+}
